b1866926579b92334b2ee69903e014e2371f1d58
MisaghMomeniB <jenabemisagh@gmail.com>
2025-04-07 12:24:07 +0330

    public transaction, string:

--- src/main.java
+++ src/main.java
@@ -6,3 +6,10 @@ class Transaction {
     String category;
     double amount;
     String date;
+
+    public Transaction(String type, String category, double amount, String date) {
+        this.type = type;
+        this.category = category;
+        this.amount = amount;
+        this.date = date;
+    }
